22873009974020ffb0e81a38d72b174e205c8a5c
Surya-SR <suryamicroservices@gmail.com>
2023-08-10 07:53:56 +0530

    updated jobs

--- app.py
+++ app.py
@@ -2,9 +2,28 @@ from flask import Flask, render_template
 
 app=Flask(__name__)
 
+job=[
+    {
+        'id':'1',
+        'title':'Data Analyst',
+        'location':'banglore',
+        'salary':'$1200'
+    },
+    {
+        'id':'2',
+        'title':'Software Enginerr',
+        'location':'Chennai'
+    },
+    {
+        'id':'3',
+        'title':'Frontend Engineer',
+        'location':'Hyderabad'
+    }
+]
+
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return render_template('index.html',JOB=job)
     
     
 if __name__=='__main__':
